8653da9b3e6e463367067f93e58fc70e76822a57
wmcosta <wmcosta@icatuseguros.com.br>
2021-06-20 18:46:23 -0300

    creation of 3 mehtods and unit tests
    creation of 3 mehtods
    
    - GetOutlier
    This method receive the array entirely comprised of odd integers or entirely comprised of even integers except for a single integer N.
    This method returns the N.
    
    - GetIntegersFromList
    The method receive a list of non-negative integers and strings and returns a new list with the strings filtered out.
    
    - FormatWords
    The method formats the words into a single comma separated value. The last word is separated by the word 'and'.
    
    Also, I created the unit tests for the 3 methods.

---
+++ ListProcessing.Tests/ListProcessApplicationTest.cs
@@ -0,0 +1,119 @@
+﻿
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace ListProcessing.Tests
+{
+    public class ListProcessApplicationTest
+    {
+       
+        [InlineData(new int[] { 1, 12, 2, 6, 4, 8, 12, 24, 22, 18, 16, 84, 1520, 2420 },1)]
+        [InlineData(new int[] { 12, 3, 5, 1, 7, 9, 11, 13, 19, 21, 25, 27, 29 }, 12)]
+        [InlineData(new int[] { 99, 2, 5}, 2)]
+        [InlineData(new int[] { 2,99, 5 }, 2)]
+        [InlineData(new int[] { 2, 4, 5 }, 5)]
+        [Trait("GetOutlier", "return the outlier")]
+        [Theory]
+        public async Task GetOutlier_ListProcess_Apllication_Return_The_Outlier(int[] arrayTest, int correctOutlier)
+        {
+
+            var listTest = new ListProcessApllication();
+
+            var result = await listTest.GetOutlier(arrayTest);
+
+            Assert.Equal(correctOutlier, result);
+        }
+
+
+        [Trait("GetOutlier", "returns null because the array is less than 3")]
+        [Fact(DisplayName = "Returns null")]
+        public async Task GetOutlier_ListProcess_Apllication_array_Less_Than_3()
+        {
+            var arrayTest = new int[] { 12, 3};
+
+            var listTest = new ListProcessApllication();
+
+            var result = await listTest.GetOutlier(arrayTest);
+
+            Assert.Null(result);
+        }
+
+        [InlineData(new int[] { 44, 12, 2, 6, 4, 8, 12, 24, 22, 18, 16, 84, 1520, 2420 })]
+        [InlineData(new int[] { 55, 3, 5, 1, 7, 9, 11, 13, 19, 21, 25, 27, 29 })]
+        [InlineData(new int[] { 99, 93, 5 })]
+        [InlineData(new int[] { 79, 99, 5 })]
+        [InlineData(new int[] { 2, 4, 1444 })]
+        [Trait("GetOutlier", "Return null - outlier not informed")]
+        [Theory]
+        public async Task GetOutlier_ListProcess_Apllication_array_outlier_not_informed_array(int[] arrayTest)
+        {
+
+            var listTest = new ListProcessApllication();
+
+            var result = await listTest.GetOutlier(arrayTest);
+
+            Assert.Null(result);
+        }
+
+
+        [Trait("GetIntegersFromList", "Return null - Empty list")]
+        [Fact]
+        public async Task GetIntegersFromList_Empty()
+        {
+            var listTest = new ListProcessApllication();
+
+            var result = await listTest.GetIntegersFromList(new List<object>());
+
+            Assert.Empty(result);
+        }
+
+
+        [Trait("GetIntegersFromList", "Return only integers")]
+        [Fact]
+        public async Task GetIntegersFromList_Return_Only_Integers()
+        {
+            var correctList = new List<object>() { 1, "banana", "Test"};
+
+            var listTest = new ListProcessApllication();
+
+            var result = await listTest.GetIntegersFromList(correctList);
+
+            Assert.Single(result);
+
+            Assert.Equal(1, result.FirstOrDefault());
+        }
+
+
+
+        [Trait("FormatWords", "return empty string")]
+        [Fact]
+        public async Task FormatWords_Return_Empty_String()
+        {
+            var stringList = new string[]{};
+
+            var listTest = new ListProcessApllication();
+
+            var result = await listTest.FormatWords(stringList);
+
+            Assert.Empty(result);
+        }
+
+        [Theory]
+        [InlineData(new string[] { "ninja", "samurai", "ronin" },"ninja, samurai and ronin")]
+        [InlineData(new string[] { "ninja", "", "ronin" }, "ninja and ronin")]
+        [Trait("FormatWords", "returns formatted string")]
+        public async Task FormatWords_Returns_Formatted_String (string[] stringList, string correctResult )
+        {
+            var listTest = new ListProcessApllication();
+
+            var result = await listTest.FormatWords(stringList);
+
+            Assert.Equal(correctResult, result);
+        }
+
+
+    }
+
+}
---
+++ ListProcessing/ListProcessingApplication.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace ListProcessing
+{
+    public  class ListProcessApllication
+    {
+
+        public static void Main()
+        {
+
+
+
+        }
+
+        public async Task<int?> GetOutlier(int[] array)
+        {
+
+            if (array.Length < 3)
+                return null;
+
+            for (int item = 0; item < array.Length; item++)
+            {
+                // if the sum of the first and last position is odd, it means that one of them is the outlier.
+                if ((array[item] + array[array.Length - item - 1]) % 2 != 0)
+                {
+                    if ((array[item] + array[item + 1]) % 2 != 0)
+                    {
+                        return array[item];
+                    }
+                    else
+                    {
+                        return array[array.Length - item - 1];
+                    }
+                }
+
+                if (((array.Length - item) - item == 3)
+                    && item + (array.Length - item )  == array.Length
+                    && ((array[item] + array[item + 1]) % 2 != 0))
+                {
+                    return array[item + 1];
+                }
+
+
+            }
+            return null;
+
+        }
+
+        public async Task<List<object>> GetIntegersFromList(List<object> list)
+        {
+
+            if (list.Count == 0)
+               return list;
+
+            var regex = new Regex("^[0-9]+$");
+
+            return list.Where(x => regex.IsMatch(x.ToString())).ToList();
+        }
+
+
+        public async Task<string> FormatWords(string[] list)
+        {
+            if (list.Length == 0)
+                return string.Empty;
+
+
+            var listFiltered = list.Where(x => !String.IsNullOrEmpty(x)).ToArray();
+
+            StringBuilder builder = new StringBuilder();
+             
+            for (int item = 0; item < listFiltered.Length ; item++)
+            {
+                builder.Append(listFiltered[item]);
+
+                if(item > listFiltered.Length - 3 && item < listFiltered.Length - 1)
+                {
+                    builder.Append(" and ");
+                }
+                else if(item < listFiltered.Length - 1)
+                {
+                    builder.Append(", ");
+                }
+            }
+
+            return builder.ToString();
+        }
+
+
+
+
+    }
+}
